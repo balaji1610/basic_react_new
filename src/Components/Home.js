02ac95a0c01e05cbd2f48538ed20f8de8d46357c
balaji1610 <balajiece1610@gmail.com>
2022-03-12 01:57:09 +0530

    Testing

---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>BASIC REACT</h1>
+        <h1> Hello BALAJI</h1>
+        <h1> Hello BALAJI</h1>
+        <button type="button" class="btn btn-primary">
+          Primary
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Home;
