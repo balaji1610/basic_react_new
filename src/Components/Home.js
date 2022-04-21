db0530df4717676df24d57948dc3d2bae40f8cb3
balaji1610 <balajiece1610@gmail.com>
2022-04-21 11:38:58 +0530

    Add CSS

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+
+import './Components.css'
+
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <h1>BASIC REACT</h1>
+        <h1 class="basicReact">BASIC REACT</h1>
         <h1> Hello BALAJI</h1>
         <h1> Hello BALAJI</h1>
         <button type="button" class="btn btn-primary">
