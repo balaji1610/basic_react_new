db30bd93cb1c155a98104c96f558b4396bc8660a
balaji1610 <balajiece1610@gmail.com>
2022-04-21 18:36:56 +0530

    Hooks

---
+++ src/Components/Hooks.js
@@ -0,0 +1,56 @@
+
+import React, { useState } from "react";
+
+
+function Hooks(){
+
+const [myself,Setmyself] = useState({
+
+
+    says:"Good Morning",
+    name:"Balaji",
+    image:'https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/200355617/original/57e33b448a8da9664e391a3b4e4d2f50e1d67dbe/be-your-front-end-developer.jpg'
+
+});
+
+
+const afterClick = function (){
+    Setmyself({
+        says:"Good Evening",
+        name:"Alex",
+        image:"https://miro.medium.com/max/1200/1*hYSKyofnqThnPIsYRfnUUQ.png"
+
+    });
+ 
+    setInterval(function(){
+        window.location.reload();
+      }, 1000);
+}
+
+
+return(
+
+    <div>
+
+<div class="card cardC" >
+  <img src={myself.image} class="card-img-top " alt="NotDisplay"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">{myself.says},Hello {myself.name}</p>
+
+    <button  class="btn btn-primary btnAlign" onClick ={afterClick}>Hooks</button>
+  </div>
+</div>
+
+
+
+
+    </div>
+)
+
+}
+
+
+export default Hooks  
+
+
--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import FunctionComponent from "./Components/FunctionComponent";
 import Props from "./Components/Props";
 
 import State from "./Components/State";
+
+import Hooks from "./Components/Hooks";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(<Home />, document.getElementById("root"));
@@ -18,6 +20,8 @@ ReactDOM.render (<FunctionComponent/>,document.getElementById("FunctionComponent
 ReactDOM.render (<Props name="balaji" developer="MERN" />,document.getElementById("Props"));
 
 ReactDOM.render( <State/>,document.getElementById("State"));
+
+ReactDOM.render(<Hooks/>,document.getElementById("Hooks"))
 
 // ReactDOM.render(
 //   <React.StrictMode>
