7b4197d458278efa1c4f8ec8693de50d190c017f
balaji1610 <balajiece1610@gmail.com>
2022-04-21 13:25:51 +0530

    Add image

--- src/Components/Functioncomponent.js
+++ src/Components/Functioncomponent.js
@@ -1,23 +1,22 @@
 import React from "react";
 
-import "./css/Components.css"
+import "./css/Components.css";
 
-
+import  mern from "./Images/mern.png";
 
 function FunctionComponent (){
-
-
-
- return (
+return (
 
 <section name="FunctionComponent">
         <div>
+        <img src = { mern } alt="mernStack" class="imgSize" /> 
+
          <h1 class="functionC">FUNCTION COMPONENT</h1>
+
+        
         </div>
         </section>
-      
-
-    )
+      )
 }
 
 export default FunctionComponent;
