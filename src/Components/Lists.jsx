82d541fe6fb365988f4ae76fff60509c348ceccf
balaji1610 <balajiece1610>
2022-05-30 23:30:08 +0530

    Member Props

--- src/Components/Lists.jsx
+++ src/Components/Lists.jsx
@@ -3,15 +3,30 @@ import react from "react";
 const Lists = () => {
   var array = ["Balaji", "Alex", "Chitra", "Berlin", "Denver"];
 
+  const Member = (props) => {
+    return <li>Good Morning {props.members} </li>;
+  };
+
   return (
     <div>
-      <ul>
-        {array.map((items, index) => (
-          <h1>
-            {index}&nbsp;&nbsp;Hello {items}
-          </h1>
-        ))}
-      </ul>
+      <div>
+        {" "}
+        <ul>
+          {array.map((items, index) => (
+            <h1>
+              {index}&nbsp;&nbsp;Hello {items}
+            </h1>
+          ))}
+        </ul>
+      </div>
+      <div>
+        {" "}
+        <li>
+          {array.map((items2) => (
+            <Member members={items2} />
+          ))}
+        </li>
+      </div>
     </div>
   );
 };
