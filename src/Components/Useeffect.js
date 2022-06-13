78cd7f6f08ddd88722604b327ea745eeff38a1af
balaji1610 <balajiece1610>
2022-06-14 01:16:26 +0530

    UseNavigation

--- src/Components/Useeffect.js
+++ src/Components/Useeffect.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import axios from "axios";
 
+import { useNavigate } from "react-router-dom";
 const UseeffectHooks = () => {
   let [page, setPosts] = useState([]);
   useEffect(() => {
@@ -18,10 +19,25 @@ const UseeffectHooks = () => {
     getResults();
   }, []);
   console.log(page, "STATE");
+
+  const Nav = useNavigate();
+
+  const click = (e) => {
+    Nav("/FunctionComponent");
+  };
   return (
     <div>
       {/* //   <button onClick={() => setPosts("posts")}>Posts</button>
     //   <button onClick={() => setPosts("comments")}>Comment</button> */}
+      &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <button type="button" class="btn btn-secondary" onClick={() => click()}>
+        Navigation
+      </button>
     </div>
   );
 };
