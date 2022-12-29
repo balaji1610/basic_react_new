c04f0799dac1482c8526d748dff416e2047f2fc4
balaji1610 <balajiece1610>
2022-12-29 19:12:02 +0530

    Wordcount

--- src/CompetitiveProgramming/Program/Program.jsx
+++ src/CompetitiveProgramming/Program/Program.jsx
@@ -1,6 +1,7 @@
 import StringCount from "../Stringcount";
 import Maskphonenumber from "../Maskphonenumber";
 import ReverseString from "../Reversestring";
+import Wordcount from "../WordCount";
 export default function program() {
   return (
     <div>
@@ -19,13 +20,24 @@ export default function program() {
               <Maskphonenumber />
             </section>
           </div>
-          <div className="col-md-4 col-sm-4">
+          <div className="col-md-4 col-sm-4 Programm_c_ui">
             <section>
               <h5>Reverse String</h5>
 
               <ReverseString />
             </section>
           </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-4 col-sm-4 Programm_c_ui">
+            <section>
+              <h5>Wordcount</h5>
+              <Wordcount />
+            </section>
+          </div>
+          <div className="col-md-4 col-sm-4"></div>
+          <div className="col-md-4 col-sm-4"></div>
         </div>
       </div>
     </div>
--- src/CompetitiveProgramming/WordCount.jsx
+++ src/CompetitiveProgramming/WordCount.jsx
@@ -1,2 +1,25 @@
+export default function Wordcount() {
+  const checkworkcount = (el) => {
+    let obj = {};
+    for (let list of el.split(" ")) {
+      if (obj[list]) {
+        obj[list]++;
+      } else {
+        obj[list] = 1;
+      }
+    }
+    // console.log(obj, "obj");
+    return obj;
+  };
 
-
+  let count = Object.entries(checkworkcount("Hello Balaji Hello Balaji Hello"));
+  console.log(count, "coun");
+  return (
+    <div>
+      {count.map((item) => {
+        console.log(item, "ggg");
+        return <p>{item}</p>;
+      })}
+    </div>
+  );
+}
