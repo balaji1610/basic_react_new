708c33387829a896fa4702bb12c8d98480266e24
balaji1610 <balajiece1610>
2022-12-29 17:57:51 +0530

    Mask_mobile_number

---
+++ src/CompetitiveProgramming/Maskphonenumber.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+export default function Maskphonenumber() {
+  const [getNumber, setGetNumber] = useState("");
+  const [result, setresult] = useState("");
+
+  const [error, setError] = useState({
+    Notnumber: false,
+    Maximumnumber: false,
+    button: false,
+  });
+
+  const handlechange = (e) => {
+    setGetNumber(e.target.value);
+    setError({
+      ...error,
+      Notnumber: false,
+      Maximumnumber: false,
+      button: false,
+    });
+
+    if (isNaN(getNumber)) {
+      setError({ ...error, Notnumber: true, button: true });
+      console.log("Not A Number");
+    } else {
+      if (getNumber.length == 10) {
+        setError({ ...error, Maximumnumber: true, button: true });
+        console.log("It is a Number");
+      }
+    }
+  };
+
+  const Masknumber = () => {
+    const firstData = getNumber.slice(-5);
+
+    const maskresult = firstData.padStart(getNumber.length, "#");
+
+    return setresult(maskresult);
+  };
+
+  //   console.log(error.Notnumber, "<---");
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={handlechange}
+        value={getNumber}
+        placeholder="MobileNumber"
+        maxlength="10"
+      />
+      <button onClick={error.button ? null : Masknumber}>
+        <span className={error.button ? "Notallowed" : null}>Submit</span>
+      </button>
+
+      <p>{result}</p>
+      <p>
+        {error.Notnumber ? (
+          <p className="Error_msg">Please Enter Numbr</p>
+        ) : null}
+      </p>
+      <p>
+        {error.Maximumnumber ? (
+          <p className="Error_msg">Please Enter 10 digit Number</p>
+        ) : null}
+      </p>
+    </div>
+  );
+}
--- src/CompetitiveProgramming/Program/Program.jsx
+++ src/CompetitiveProgramming/Program/Program.jsx
@@ -1,11 +1,26 @@
 import StringCount from "../Stringcount";
+import Maskphonenumber from "../Maskphonenumber";
 export default function program() {
   return (
     <div>
-      <section className="Programm_c_ui">
-        <h1>StringCount</h1>
-        <StringCount />
-      </section>
+      <div className="col-md-12 col-sm-12">
+        <div className="row">
+          <div className="col-md-4 col-sm-4 Programm_c_ui">
+            <section>
+              <h5>StringCount</h5>
+              <StringCount />
+            </section>
+          </div>
+          <div className="col-md-4 col-sm-4 Programm_c_ui ">
+            <section>
+              <h5>Maskphonenumber</h5>
+
+              <Maskphonenumber />
+            </section>
+          </div>
+          <div className="col-md-4 col-sm-4">44</div>
+        </div>
+      </div>
     </div>
   );
 }
