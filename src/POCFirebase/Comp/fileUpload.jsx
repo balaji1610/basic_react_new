dd0b1edc67f0356e32984d501e0847a42be73f2e
balaji1610 <balajiece1610@gmail.com>
2023-04-06 14:57:47 +0530

    causorl

--- src/POCFirebase/Comp/fileUpload.jsx
+++ src/POCFirebase/Comp/fileUpload.jsx
@@ -5,6 +5,8 @@ function App() {
   // State to store uploaded file
   const [file, setFile] = useState(""); // progress
   const [percent, setPercent] = useState(0); // Handle file upload event and update state
+
+  const [image, setImage] = useState([]);
   function handleChange(event) {
     setFile(event.target.files[0]);
   }
@@ -26,18 +28,78 @@ function App() {
       () => {
         // download url
         getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          setImage((state) => state.concat(url));
+
           console.log(url);
         });
       }
     );
   };
+  console.log(image, "image");
+
   return (
     <div>
-                  
       <input type="file" onChange={handleChange} accept="/image/*" />
-                  <button onClick={handleUpload}>Upload to Firebase</button>
-                  <p>{percent} "% done"</p>
-              
+      <button onClick={handleUpload}>Upload to Firebase</button>
+      <p>{percent} "% done"</p> 
+      {/* <section>
+        <br />
+        <br />
+        {image.map((val) => (
+          <img src={val} alt="example" />
+        ))}
+      </section> */}
+      <section>
+        <div
+          id="carouselExampleIndicators"
+          class="carousel slide"
+          data-bs-ride="carousel"
+          data-interval="10000"
+        >
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img
+                src="https://firebase.google.com/images/social.png"
+                class="d-block w-100"
+                width="500"
+                height="350"
+                alt="01"
+              />
+            </div>
+            <div>
+              {image.map((val) => (
+                <div class="carousel-item">
+                  <img
+                    src={val}
+                    class="d-block w-100"
+                    width="500"
+                    height="350"
+                    alt="mappingImage"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </section>
     </div>
   );
 }
