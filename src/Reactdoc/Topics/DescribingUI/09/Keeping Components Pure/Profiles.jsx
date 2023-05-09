8835cbd656d3059a30aad3d6da880a7aca66f773
balaji1610 <balajiece1610@gmail.com>
2023-05-10 01:08:49 +0530

    Utills_list

--- src/Reactdoc/Topics/DescribingUI/09/Keeping Components Pure/Profiles.jsx
+++ src/Reactdoc/Topics/DescribingUI/09/Keeping Components Pure/Profiles.jsx
@@ -1,19 +1,6 @@
 import CardProfile from "./ConsProfile/CardProfile";
+import { list } from "./ConsProfile/List";
 export default function Profiles() {
-  const list = [
-    {
-      imageUrl:
-        "https://www.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-600w-407021107.jpg",
-      name: "Nautural",
-    },
-
-    {
-      imageUrl:
-        "https://www.oxfordwebstudio.com/user/pages/06.da-li-znate/sta-je-html/sta-je-html.jpg",
-      name: "HTML",
-    },
-  ];
-
   const [one, two] = list;
 
   return (
