24876d538c86a3b44a165eafafe1eadcc7c68047
balaji1610 <balajiece1610@gmail.com>
2023-06-09 22:57:11 +0530

    Grid

--- src/MaterialUI/Pages/Layout.jsx
+++ src/MaterialUI/Pages/Layout.jsx
@@ -1,12 +1,15 @@
 import BoxTopic from "../Topices/LayoutTopic/BoxTopic";
 import ContainerTopic from "../Topices/LayoutTopic/ContainerTopic";
+import GridTopic from "../Topices/LayoutTopic/GridTopic";
 export default function Layout() {
   return (
     <div>
-      <h1>BOX</h1>
+      <h1 style={{ textAlign: "center" }}>BOX</h1>
       <BoxTopic />
-      <h1>Container</h1>
+      <h1 style={{ textAlign: "center" }}>Container</h1>
       <ContainerTopic />
+      <h1 style={{ textAlign: "center" }}>Grid</h1>
+      <GridTopic />
     </div>
   );
 }
---
+++ src/MaterialUI/Topices/LayoutTopic/GridTopic.jsx
@@ -0,0 +1,173 @@
+import Grid from "@mui/material/Grid";
+import { Button } from "@mui/material";
+export default function GridTopic() {
+  return (
+    <div>
+      <h1>Grid Systems</h1>
+      <Grid container columns={12} spacing={5}>
+        <Grid item xs={8}>
+          <div className="itemsGrid">xs=8</div>
+        </Grid>
+        <Grid item xs={4}>
+          <div className="itemsGrid">xs=4</div>
+        </Grid>
+        <div></div>
+        <Grid item xs={4}>
+          <div className="itemsGrid">xs=4</div>
+        </Grid>
+        <Grid item xs={8}>
+          <div className="itemsGrid">xs=8</div>
+        </Grid>
+
+        <>
+          {" "}
+          <Grid item xs={4}>
+            <div className="itemsGrid">xs=4</div>
+          </Grid>
+          <Grid item xs={4}>
+            <div className="itemsGrid">xs=4</div>
+          </Grid>
+          <Grid item xs={4}>
+            <div className="itemsGrid">xs=4</div>
+          </Grid>
+        </>
+
+        <>
+          <Grid item xs={3}>
+            <div className="itemsGrid">xs=3</div>
+          </Grid>
+          <Grid item xs={3}>
+            <div className="itemsGrid">xs=3</div>
+          </Grid>
+          <Grid item xs={3}>
+            <div className="itemsGrid">xs=3</div>
+          </Grid>
+          <Grid item xs={3}>
+            <div className="itemsGrid">xs=3</div>
+          </Grid>
+        </>
+
+        <>
+          {" "}
+          <Grid item xs={2}>
+            <div className="itemsGrid">xs=2</div>
+          </Grid>
+          <Grid item xs={2}>
+            <div className="itemsGrid">xs=2</div>
+          </Grid>
+          <Grid item xs={2}>
+            <div className="itemsGrid">xs=2</div>
+          </Grid>
+          <Grid item xs={2}>
+            <div className="itemsGrid">xs=2</div>
+          </Grid>
+          <Grid item xs={2}>
+            <div className="itemsGrid">xs=2</div>
+          </Grid>
+          <Grid item xs={2}>
+            <div className="itemsGrid">xs=2</div>
+          </Grid>
+        </>
+      </Grid>
+
+      <div className="divder"></div>
+      <h1 style={{ textAlign: "center" }}>Flexbox</h1>
+      <Grid container columns={12} spacing={5}>
+        <Grid
+          item
+          direction="column"
+          justifyContent="space-between"
+          alignItems="flex-start"
+          flexWrap="wrap"
+          xs={8}
+        >
+          <div>
+            <h1>App Development Done with Lastest Technology</h1>
+          </div>
+          <div>
+            <h6>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              mollitia, molestiae quas vel sint commodi repudiandae
+            </h6>
+          </div>
+          <div>
+            <Button>contact us</Button>
+          </div>
+        </Grid>
+        <Grid item xs={4}>
+          <div>
+            {" "}
+            <img
+              src="https://www.udaantechnologies.com/images/mobile-banner/m-mobile-website-design-and-development-banner-icon.png"
+              width="200vh"
+              height="300vh"
+            />
+          </div>
+        </Grid>
+      </Grid>
+
+      <div className="divder"></div>
+      <h1 style={{ textAlign: "center" }}>Columns 16</h1>
+
+      <Grid container columns={16} spacing={5}>
+        <Grid item xs={8}>
+          <div className="itemsGrid">xs=8</div>
+        </Grid>
+        <Grid item xs={8}>
+          <div className="itemsGrid">xs=8</div>
+        </Grid>
+        <Grid item xs={4}>
+          <div className="itemsGrid">xs=4</div>
+        </Grid>
+        <Grid item xs={4}>
+          <div className="itemsGrid">xs=4</div>
+        </Grid>
+        <Grid item xs={4}>
+          <div className="itemsGrid">xs=4</div>
+        </Grid>
+        <Grid item xs={4}>
+          <div className="itemsGrid">xs=4</div>
+        </Grid>
+        <Grid item xs={3}>
+          <div className="itemsGrid">xs=3</div>
+        </Grid>
+        <Grid item xs={3}>
+          <div className="itemsGrid">xs=3</div>
+        </Grid>
+        <Grid item xs={3}>
+          <div className="itemsGrid">xs=3</div>
+        </Grid>
+        <Grid item xs={3}>
+          <div className="itemsGrid">xs=3</div>
+        </Grid>
+        <Grid item xs={3}>
+          <div className="itemsGrid">xs=3</div>
+        </Grid>
+        <Grid item xs={2}>
+          <div className="itemsGrid">xs=2</div>
+        </Grid>
+        <Grid item xs={2}>
+          <div className="itemsGrid">xs=2</div>
+        </Grid>
+        <Grid item xs={2}>
+          <div className="itemsGrid">xs=2</div>
+        </Grid>
+        <Grid item xs={2}>
+          <div className="itemsGrid">xs=2</div>
+        </Grid>
+        <Grid item xs={2}>
+          <div className="itemsGrid">xs=2</div>
+        </Grid>
+        <Grid item xs={2}>
+          <div className="itemsGrid">xs=2</div>
+        </Grid>
+        <Grid item xs={2}>
+          <div className="itemsGrid">xs=2</div>
+        </Grid>
+        <Grid item xs={2}>
+          <div className="itemsGrid">xs=2</div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
