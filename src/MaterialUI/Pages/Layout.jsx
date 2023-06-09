8b6b64d66295e2b2c7f9ad41ca693c7e4d750ce5
balaji1610 <balajiece1610@gmail.com>
2023-06-09 15:39:10 +0530

    container

--- src/MaterialUI/Pages/Layout.jsx
+++ src/MaterialUI/Pages/Layout.jsx
@@ -1,10 +1,12 @@
 import BoxTopic from "../Topices/LayoutTopic/BoxTopic";
-
+import ContainerTopic from "../Topices/LayoutTopic/ContainerTopic";
 export default function Layout() {
   return (
     <div>
-      <h1>Layout</h1>
+      <h1>BOX</h1>
       <BoxTopic />
+      <h1>Container</h1>
+      <ContainerTopic />
     </div>
   );
 }
---
+++ src/MaterialUI/Topices/LayoutTopic/ContainerTopic.jsx
@@ -0,0 +1,13 @@
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+export default function ContainerTopic() {
+  return (
+    <div>
+      <Container maxWidth="xl" style={{ backgroundColor: "red" }}>
+        <Box sx={{ height: "50vh" }}>
+          <div>balaji</div>
+        </Box>
+      </Container>
+    </div>
+  );
+}
